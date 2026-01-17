207b231b0a132a456a6e0caadfb57bcc1e8a209a
Jack Sparrow <novalgans1908@gmail.com>
2026-01-18 02:14:38 +0700

    increase DisplayOrder to 10000 to overlay TopbarPlus

--- EnvielUI.lua
+++ EnvielUI.lua
@@ -138,7 +138,7 @@ function EnvielUI:CreateWindow(Config)
 	local ScreenGui = Create("ScreenGui", {
 		Name = "EnvielUI", Parent = Parent, 
 		ZIndexBehavior = Enum.ZIndexBehavior.Sibling, ResetOnSpawn = false,
-		IgnoreGuiInset = true
+		IgnoreGuiInset = true, DisplayOrder = 10000
 	})
 	
 	local MainFrame = Create("Frame", {
